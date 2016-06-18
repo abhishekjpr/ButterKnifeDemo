87fcfa4d177c2ef76f609dffa8ac825973282306
abhishekjpr <abhishekjpr@outlook.com>
2016-06-18 21:44:12 +0530

    Updated

---
+++ app/src/main/java/com/example/abhishekjpr/butterknifedemo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.abhishekjpr.butterknifedemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textview)
+    TextView textview;
+    @BindView(R.id.button)
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Log.d("Butto Ref :: ", button+"");
+    }
+
+    @OnClick(R.id.button)
+    public void changeData(){
+        textview.setText("You have change data..!!");
+    }
+}
